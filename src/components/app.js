7c2d03a00cf34d0db30997bb702a98c68163b212
Christopher Astfalk <christopher.astfalk@icloud.com>
2019-01-05 09:35:15 +0100

    add License note to all files

--- src/components/app.js
+++ src/components/app.js
@@ -1,3 +1,10 @@
+/*
+License:
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
+the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 import { h, Component } from 'preact'
 import { Router } from 'preact-router'
 import Hammer from 'hammerjs'
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,9 @@
+/*
+License:
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
+the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
 
 export function getDaysInMonth (year, month) {
   // first day in month is 1. 0 is the one before --> last day of month!
