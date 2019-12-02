5daf8c52a123e493ad5be7884317fc1d9022f197
Christopher Astfalk <christopher.astfalk@icloud.com>
2019-12-02 18:20:06 +0100

    feat(download): add ics files for 6-4 shift model

--- src/components/download/index.js
+++ src/components/download/index.js
@@ -8,7 +8,7 @@ the MPL was not distributed with this file, You can obtain one at http://mozilla
 import { h } from 'preact'
 import style from './style.less'
 
-import { shiftModelText, shift66Name } from '../../lib/constants'
+import { shiftModelText, shift66Name, shift64Name } from '../../lib/constants'
 
 import cal_6_6_group_1 from '../../assets/Schichtgruppe_1.ics'
 import cal_6_6_group_2 from '../../assets/Schichtgruppe_2.ics'
@@ -16,6 +16,12 @@ import cal_6_6_group_3 from '../../assets/Schichtgruppe_3.ics'
 import cal_6_6_group_4 from '../../assets/Schichtgruppe_4.ics'
 import cal_6_6_group_5 from '../../assets/Schichtgruppe_5.ics'
 import cal_6_6_group_6 from '../../assets/Schichtgruppe_6.ics'
+
+import cal_6_4_group_1 from '../../assets/6-4_gruppe_1.ics'
+import cal_6_4_group_2 from '../../assets/6-4_gruppe_2.ics'
+import cal_6_4_group_3 from '../../assets/6-4_gruppe_3.ics'
+import cal_6_4_group_4 from '../../assets/6-4_gruppe_4.ics'
+import cal_6_4_group_5 from '../../assets/6-4_gruppe_5.ics'
 
 const urls = {
   [shift66Name]: [
@@ -25,6 +31,13 @@ const urls = {
     cal_6_6_group_4,
     cal_6_6_group_5,
     cal_6_6_group_6
+  ],
+  [shift64Name]: [
+    cal_6_4_group_1,
+    cal_6_4_group_2,
+    cal_6_4_group_3,
+    cal_6_4_group_4,
+    cal_6_4_group_5
   ]
 }
 
