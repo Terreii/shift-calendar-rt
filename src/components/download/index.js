b5847288186dcc515a567cd6072014f5b99ee029
Christopher Astfalk <christopher.astfalk@icloud.com>
2019-12-02 18:46:02 +0100

    feat(download): add ics files for fire department

--- src/components/download/index.js
+++ src/components/download/index.js
@@ -8,7 +8,7 @@ the MPL was not distributed with this file, You can obtain one at http://mozilla
 import { h } from 'preact'
 import style from './style.less'
 
-import { shiftModelText, shift66Name, shift64Name } from '../../lib/constants'
+import { shiftModelText, shift66Name, shift64Name, shiftWfW } from '../../lib/constants'
 
 import cal_6_6_group_1 from '../../assets/Schichtgruppe_1.ics'
 import cal_6_6_group_2 from '../../assets/Schichtgruppe_2.ics'
@@ -22,6 +22,13 @@ import cal_6_4_group_2 from '../../assets/6-4_gruppe_2.ics'
 import cal_6_4_group_3 from '../../assets/6-4_gruppe_3.ics'
 import cal_6_4_group_4 from '../../assets/6-4_gruppe_4.ics'
 import cal_6_4_group_5 from '../../assets/6-4_gruppe_5.ics'
+
+import cal_wfw_group_1 from '../../assets/wfw_gruppe_1.ics'
+import cal_wfw_group_2 from '../../assets/wfw_gruppe_2.ics'
+import cal_wfw_group_3 from '../../assets/wfw_gruppe_3.ics'
+import cal_wfw_group_4 from '../../assets/wfw_gruppe_4.ics'
+import cal_wfw_group_5 from '../../assets/wfw_gruppe_5.ics'
+import cal_wfw_group_6 from '../../assets/wfw_gruppe_6.ics'
 
 const urls = {
   [shift66Name]: [
@@ -38,12 +45,22 @@ const urls = {
     cal_6_4_group_3,
     cal_6_4_group_4,
     cal_6_4_group_5
+  ],
+  [shiftWfW]: [
+    cal_wfw_group_1,
+    cal_wfw_group_2,
+    cal_wfw_group_3,
+    cal_wfw_group_4,
+    cal_wfw_group_5,
+    cal_wfw_group_6
   ]
 }
 
 export default ({ shiftModel }) => {
   if (!(shiftModel in urls)) {
-    return <div class={style.main}>Für dieses Schichtmodell sind die Kalender noch in Arbeit</div>
+    return <div class={style.main + ' ' + style.todo}>
+      Für dieses Schichtmodell sind die Kalender noch in Arbeit
+    </div>
   }
 
   return <div class={style.main}>
