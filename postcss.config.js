c941c9a52f825cde7215827cee2fa3605fe0573f
Christopher Astfalk <christopher.astfalk@icloud.com>
2020-06-09 11:20:53 +0200

    chore(tailwindcss): use purge in tailwind config

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   plugins: [
     require('tailwindcss'),
-    require('autoprefixer'),
-    process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
-      content: [
-        './src/components/*.js',
-        './src/components/**/*.js',
-        './src/index.html'
-      ],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    })
+    require('autoprefixer')
   ]
 }
