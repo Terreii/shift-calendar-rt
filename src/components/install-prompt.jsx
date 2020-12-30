09240e6656f43e77e12fa272c536babd3abb74e5
Christopher Astfalk <christopher.astfalk@icloud.com>
2020-12-30 11:21:04 +0100

    feat(install-prompt): better web-manifest install prompt

--- src/components/install-prompt.jsx
+++ src/components/install-prompt.jsx
@@ -32,7 +32,9 @@ export default function InstallButton () {
         // Stash the event so it can be triggered later.
         deferredPrompt.current = event
 
-        setShow('button')
+        if (dismissedTime < (Date.now() - ms.days(12))) {
+          setShow('button')
+        }
       }
 
       /**
@@ -74,17 +76,33 @@ export default function InstallButton () {
   switch (show) {
     case 'button':
       return (
-        <div
-          class='fixed bottom-0 left-0 bg-gray-200 text-base border-t border-gray-500 py-1 shadow pb-safe-area focus-within:bg-gray-400 hover:bg-gray-300 focus-within:ring'
+        <aside
+          class='fixed bottom-0 left-0 flex flex-row md:mb-2 md:ml-2 max-w-screen-sm md:max-w-sm bg-gray-200 text-base border rounded-t-lg md:rounded-lg border-gray-500 shadow pb-safe-area'
         >
-          <button
-            className='flex flex-row items-center justify-center w-screen h-8 text-black focus:outline-none'
-            onClick={onClickInstallButton}
-          >
-            <img src='/assets/icons/add-outline.svg' height='30' width='30' alt='' />
-            <span class='ml-2'>zum Home Screen hinzufügen / installieren</span>
-          </button>
-        </div>
+          <p className='p-4'>
+            Dieser Kalender kann wie eine <em>App installiert</em> werden!
+            <em> Ohne auf deine Daten zugreifen zu können!</em>
+            <br />
+            Klicke auf installieren um ihn zu deinen Apps hinzu zufügen.
+          </p>
+          <div className='flex flex-col border-l border-black'>
+            <button
+              className='flex flex-col items-center justify-center flex-grow px-2 py-1 text-black hover:bg-gray-300 focus:bg-gray-400 focus:ring focus:outline-none'
+              onClick={onClickInstallButton}
+            >
+              <img src='/assets/icons/add-outline.svg' height='30' width='30' alt='' />
+              <span className='mt-1'>installieren</span>
+            </button>
+            <button
+              type='button'
+              onClick={dismiss}
+              title='Klicke um den Kalender nicht zu installieren'
+              className='flex flex-col items-center justify-center flex-grow px-2 py-1 border-t border-black hover:bg-gray-300 focus:bg-gray-400 focus:ring focus:outline-none'
+            >
+              Abbrechen
+            </button>
+          </div>
+        </aside>
       )
 
     case 'ios':
@@ -93,7 +111,7 @@ export default function InstallButton () {
           class='fixed bottom-0 w-screen flex flex-col items-center bg-gray-200 border-t border-gray-500 shadow-lg pb-safe-area'
         >
           <span class='text-sm text-gray-900 text-center my-1'>
-            Klicke auf Teilen &amp; dann "Zum Home-Bildschirm" um den Kalender zum installieren:
+            Klicke auf Teilen &amp; dann "Zum Home-Bildschirm" um den Kalender zu installieren:
           </span>
           <div class='flex flex-row items-stretch mb-1'>
             <img
@@ -112,13 +130,7 @@ export default function InstallButton () {
               alt='klicke Zum Home-Bildschirm'
             />
           </div>
-          <button
-            class='ml-1 absolute bottom-0 right-0 bg-transparent border-0'
-            onClick={dismiss}
-            aria-label='schließe Meldung'
-          >
-            <img src='/assets/icons/close.svg' height='40' width='40' alt='' />
-          </button>
+          <CloseButton onClick={dismiss} />
         </div>
       )
 
@@ -126,4 +138,17 @@ export default function InstallButton () {
     default:
       return null
   }
+}
+
+function CloseButton ({ onClick }) {
+  return (
+    <button
+      type='button'
+      class='ml-1 absolute bottom-0 right-0 bg-transparent border-0'
+      onClick={onClick}
+      aria-label='schließe Meldung'
+    >
+      <img src='/assets/icons/close.svg' height='40' width='40' alt='' />
+    </button>
+  )
 }
