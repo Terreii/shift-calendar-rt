3138d2ba83583e0b9c7319505259837548708c32
Christopher Astfalk <christopher.astfalk@icloud.com>
2021-01-02 21:17:15 +0100

    perf(build): add snowpack optimize and fix vercel

--- snowpack.config.js
+++ snowpack.config.js
@@ -9,12 +9,23 @@ module.exports = {
   buildOptions: {
     baseUrl: '/'
   },
+  exclude: [
+    '**/__mocks__/**',
+    '**/__tests__/**'
+  ],
   installOptions: {
     polyfillNode: true
   },
+  experiments: {
+    optimize: {
+      manifest: true,
+      bundle: true,
+      minify: true,
+      target: 'es2017'
+    }
+  },
   plugins: [
     '@snowpack/plugin-dotenv',
-    '@prefresh/snowpack',
-    ['@snowpack/plugin-webpack', {}]
+    '@prefresh/snowpack'
   ]
 }
