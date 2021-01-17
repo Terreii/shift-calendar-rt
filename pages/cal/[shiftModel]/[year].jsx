4a0d2a33060c4ddedfe2bcaea43831e61a33a988
Christopher Astfalk <christopher.astfalk@icloud.com>
2021-01-17 17:11:21 +0100

    feat(ssr): add SSR to year and months

--- pages/cal/[shiftModel]/[year].jsx
+++ pages/cal/[shiftModel]/[year].jsx
@@ -9,6 +9,8 @@ import { useRouter } from 'next/router'
 
 import Main from '../../../components/main'
 import { useTodayZeroIndex } from '../../../hooks/time'
+import { shiftModelNames, shiftModelNumberOfGroups } from '../../../lib/constants'
+import { getIsSSR } from '../../../lib/utils'
 
 export default function Month () {
   const router = useRouter()
@@ -19,11 +21,55 @@ export default function Month () {
     <Main
       isFullYear
       year={parseInt(router.query.year, 10)}
-      month={parseInt(router.query.month, 10)}
+      month={1}
       shiftModel={router.query.shiftModel}
-      today={today}
+      today={getIsSSR ? [1999, 0, 1, 0] : today}
       search={Number.isNaN(search) ? null : search}
       group={router.query.group}
     />
   </div>
+}
+
+export async function getStaticPaths () {
+  const now = new Date().getFullYear()
+  const years = [
+    now,
+    now - 1,
+    now + 1
+  ].map(String)
+
+  const paths = []
+
+  for (const shiftModel of shiftModelNames) {
+    // renders 0 (all group) and each group (1 to max), thats why the <= is there.
+    for (let i = 0, max = shiftModelNumberOfGroups[shiftModel]; i <= max; i++) {
+      const group = String(i)
+
+      for (const year of years) {
+        paths.push({
+          params: {
+            year,
+            shiftModel,
+            group
+          }
+        })
+      }
+    }
+  }
+
+  return {
+    paths,
+    fallback: true
+  }
+}
+
+export async function getStaticProps (props) {
+  return {
+    props: {
+      ...props,
+      locales: null,
+      locale: null,
+      defaultLocale: null
+    }
+  }
 }
--- pages/cal/[shiftModel]/[year]/[month].jsx
+++ pages/cal/[shiftModel]/[year]/[month].jsx
@@ -6,11 +6,14 @@ the MPL was not distributed with this file, You can obtain one at http://mozilla
 */
 
 import { useRouter } from 'next/router'
+import ms from 'milliseconds'
 
 import Main from '../../../../components/main'
 import { useTodayZeroIndex } from '../../../../hooks/time'
+import { shiftModelNames, shiftModelNumberOfGroups } from '../../../../lib/constants'
+import { getIsSSR } from '../../../../lib/utils'
 
-export default function Month () {
+export default function Month (props) {
   const router = useRouter()
   const today = useTodayZeroIndex()
   const search = parseInt(router.query.search, 10)
@@ -21,9 +24,58 @@ export default function Month () {
       year={parseInt(router.query.year, 10)}
       month={parseInt(router.query.month, 10)}
       shiftModel={router.query.shiftModel}
-      today={today}
+      today={getIsSSR() ? [1999, 0, 1, 0] : today}
       search={Number.isNaN(search) ? null : search}
       group={router.query.group}
     />
   </div>
+}
+
+export async function getStaticPaths () {
+  const now = new Date()
+  const months = [
+    now,
+    new Date(now.getTime() - ms.months(1)), // last month
+    new Date(now.getTime() + ms.months(1)), // next month
+    new Date(now.getTime() + ms.months(2)), // after next month
+  ]
+    .map(date => ({
+      year: String(date.getFullYear()),
+      month: String(date.getMonth() + 1).padStart(2, '0')
+    }))
+
+  const paths = []
+
+  for (const shiftModel of shiftModelNames) {
+    // renders 0 (all group) and each group (1 to max), thats why the <= is there.
+    for (let i = 0, max = shiftModelNumberOfGroups[shiftModel]; i <= max; i++) {
+      const group = String(i)
+
+      for (const date of months) {
+        paths.push({
+          params: {
+            ...date,
+            shiftModel,
+            group
+          }
+        })
+      }
+    }
+  }
+
+  return {
+    paths,
+    fallback: true
+  }
+}
+
+export async function getStaticProps (props) {
+  return {
+    props: {
+      ...props,
+      locales: null,
+      locale: null,
+      defaultLocale: null
+    }
+  }
 }
