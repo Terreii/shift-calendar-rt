9cf77433efd6cc063c58d7354f353306a2980a53
Christopher Astfalk <christopher.astfalk@icloud.com>
2022-08-29 23:08:11 +0200

    add a navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Renders a navbar-link.
+  # It sets the classes if the link is the current path.
+  def nav_link_to(text, path, options, &block)
+    is_current_path = request.path == path
+    options[:class] = class_names(options[:class], options["class"], { "current": is_current_path })
+    options.delete "class" if options.has_key? "class"
+    options["aria-current"] = is_current_path ? "page" : nil
+    link_to text, path, options, &block
+  end
 end
