3873bb645d898b34dc3ca2bec17729247de99f4c
Christopher Astfalk <christopher.astfalk@icloud.com>
2023-07-18 13:17:00 +0200

    chore(excel_export): add query param validation

--- pages/api/excel_export/all/[year]/[month].ts
+++ pages/api/excel_export/all/[year]/[month].ts
@@ -12,10 +12,26 @@ import {
 } from "../../../../../lib/excel_export";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const wb = new xl.Workbook();
   const year = parseInt(req.query.year as string, 10);
   const month = parseInt(req.query.month as string, 10);
 
+  if (
+    Number.isNaN(year) ||
+    year < 2010 ||
+    Number.isNaN(month) ||
+    month < 1 ||
+    month > 12
+  ) {
+    res.status(404).json({
+      status: 404,
+      error: "Unknown year or month",
+      describtion:
+        "This url must have a year and month! Example: /api/excel_export/all/2023/4",
+    });
+    return;
+  }
+
+  const wb = new xl.Workbook();
   const styles = createStyles(wb);
 
   for (const model of shiftModelNames) {
--- pages/api/excel_export/shift/[model]/[year].ts
+++ pages/api/excel_export/shift/[model]/[year].ts
@@ -3,6 +3,7 @@ import xl from "excel4node";
 
 import {
   monthNames,
+  shiftModelNames,
   ShiftModels,
   excelExportModelFullYearName,
 } from "../../../../../lib/constants";
@@ -12,10 +13,30 @@ import {
 } from "../../../../../lib/excel_export";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const wb = new xl.Workbook();
   const year = parseInt(req.query.year as string, 10);
   const model = req.query.model as ShiftModels;
 
+  if (Number.isNaN(year) || year < 2010) {
+    res.status(404).json({
+      status: 404,
+      error: "Unknown year",
+      describtion:
+        "This url must have a year! Example: /api/excel_export/shift/6-6/2023",
+    });
+    return;
+  }
+  if (!shiftModelNames.includes(model)) {
+    res.status(404).json({
+      status: 404,
+      error: "Unknown shift model",
+      describtion:
+        "This url must have a valid shift model! Example: /api/excel_export/shift/6-6/2023",
+      shiftModelNames,
+    });
+    return;
+  }
+
+  const wb = new xl.Workbook();
   const styles = createStyles(wb);
 
   for (let i = 0; i < 12; i++) {
