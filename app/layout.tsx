2c2780873e90e7a2d1345c8227b1452f1047f3e3
Christopher Astfalk <christopher.astfalk@icloud.com>
2023-09-10 11:47:55 +0200

    feat(app-router): move impressum to app reouter

---
+++ app/layout.tsx
@@ -0,0 +1,58 @@
+/*
+License:
+
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
+the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+import type { ReactNode } from "react";
+import type { Metadata } from "next";
+
+import "modern-css-reset";
+import "../styles/index.css";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Schichtkalender für Bosch Reutlingen",
+    default: "Schichtkalender für Bosch Reutlingen",
+  },
+  description:
+    "Inoffizieller Schichtkalender für Bosch Reutlingen. Listet alle Kontischichten von Bosch Reutlingen auf.",
+  appleWebApp: {
+    title: "Schichtkalender",
+    statusBarStyle: "default",
+  },
+  generator: "Next.js",
+  alternates: {
+    canonical: "https://schichtkalender.app/",
+  },
+  applicationName: "Schichtkalender Bosch RtP1",
+  keywords: [
+    "Schichtkalender",
+    "Bosch",
+    "Reutlingen",
+    "Kontischicht",
+    "Kalender",
+  ],
+  authors: {
+    name: "Christopher Astfalk",
+    url: "https://christopher-astfalk.de/",
+  },
+  creator: "Christopher Astfalk",
+  publisher: "Christopher Astfalk",
+  colorScheme: "light",
+  themeColor: "#064E3B",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    viewportFit: "cover",
+  },
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="de">
+      <body>{children}</body>
+    </html>
+  );
+}
