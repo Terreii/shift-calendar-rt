b92d6cd74dfc55edd57efdef87e7093de2a2980d
Christopher Astfalk <christopher.astfalk@icloud.com>
2023-09-30 00:09:00 +0200

    test(e2e): test index page

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,18 @@
 /* eslint-disable jest/expect-expect */
 
-describe("template spec", () => {
+import { shiftModelText } from "../../lib/constants";
+
+describe("basic usage", () => {
+  it("should have all shift models on index", () => {
+    cy.visit("http://localhost:3000/");
+
+    for (const [key, name] of Object.entries(shiftModelText)) {
+      cy.contains("main a", name)
+        .should("have.attr", "href")
+        .and("include", key);
+    }
+  });
+
   it("visit impressum", () => {
     cy.visit("http://localhost:3000/");
     cy.contains("Impressum").click();
