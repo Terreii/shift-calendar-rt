4c7f75ec3f4dc390778437d7f8f1f1b8cda2d9b3
Christopher Astfalk <christopher.astfalk@icloud.com>
2023-10-18 17:36:00 +0200

    test(e2e): add download tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,10 @@
 /* eslint-disable jest/expect-expect */
 
-import { shiftModelText } from "../../lib/constants";
+import {
+  shiftModelText,
+  shiftModelNames,
+  monthNames,
+} from "../../lib/constants";
 
 describe("basic usage", () => {
   it("should have all shift models on index", () => {
@@ -22,5 +26,43 @@ describe("basic usage", () => {
 
     cy.get("header h1").click();
     cy.url().should("not.include", "/impressum");
+  });
+});
+
+describe("download", () => {
+  it("should be accessable by the calendars", () => {
+    cy.visit(
+      `http://localhost:3000/cal/${
+        shiftModelNames[Math.floor(Math.random() * shiftModelNames.length)]
+      }`,
+    );
+    cy.contains("Es gibt mehr Download-Optionen!").click();
+
+    cy.url().should("include", "/download");
+
+    const now = new Date();
+    const href = `/api/excel_export/all/${now.getFullYear()}/${
+      now.getMonth() + 1
+    }`;
+    cy.contains(
+      `Alle Schichten für ${
+        monthNames[now.getMonth()]
+      } ${now.getFullYear()} als Tabelle:`,
+    )
+      .next()
+      .should("have.attr", "href")
+      .and("include", href);
+  });
+
+  it("should have a year calendar for every shift model", () => {
+    cy.visit("http://localhost:3000/download");
+    const year = new Date().getFullYear();
+
+    for (const name of shiftModelNames) {
+      cy.contains(shiftModelText[name] + ":")
+        .next()
+        .should("have.attr", "href")
+        .and("include", `/api/excel_export/shift/${name}/${year}`);
+    }
   });
 });
