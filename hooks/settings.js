486ed78155bb6ee7679e7a78e9c2ba0ba651bd1f
Christopher Astfalk <christopher.astfalk@icloud.com>
2023-11-05 22:18:09 +0100

    chore(settings): remove localStorage hooks (parts)

--- hooks/settings.js
+++ hooks/settings.js
@@ -5,14 +5,8 @@ This Source Code Form is subject to the terms of the Mozilla Public License, v. 
 the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
-import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
-import {
-  shift66Name,
-  shiftModelNames,
-  shiftModelNumberOfGroups,
-} from "../lib/constants";
 import { useToday } from "./time";
 
 /**
@@ -31,7 +25,6 @@ export function useQueryProps() {
   const router = useRouter();
 
   const {
-    asPath: url,
     query: {
       shiftModel,
       year: yearStr,
@@ -49,64 +42,12 @@ export function useQueryProps() {
   const search =
     searchStr && !Number.isNaN(searchStr) ? parseInt(searchStr, 10) : null;
 
-  const [storedSettings, setStoredSettings] = useState({
-    shiftModel: shiftModel ?? shift66Name,
-    group: Number.isNaN(group) ? 0 : group,
-  });
-  useEffect(() => {
-    const settings = JSON.parse(window.localStorage.getItem("settings")) ?? {};
-    if (settings.didSelectModel) {
-      setStoredSettings((old) => {
-        if (
-          (settings.shiftModel != null &&
-            old.shiftModel !== settings.shiftModel) ||
-          (settings.group != null && old.group !== settings.group)
-        ) {
-          return {
-            shiftModel: settings.shiftModel,
-            group: settings.group,
-          };
-        }
-        return old;
-      });
-    }
-  }, [url]);
-
   return {
-    url,
     isFullYear,
     year,
     month,
-    shiftModel: shiftModel ?? storedSettings.shiftModel,
-    group: Number.isNaN(group) ? storedSettings.group : group,
+    shiftModel: shiftModel,
+    group: Number.isNaN(group) ? 0 : group,
     search,
   };
-}
-
-/**
- * Stores the last selected shiftModel and group.
- * @param {string} url           Current active url-path.
- * @param {string} [shiftModel]  Selected shift model.
- * @param {number} group         Group to display.
- */
-export function useSaveSettings(url, shiftModel, group) {
-  useEffect(() => {
-    if (!url.startsWith("/cal")) return;
-
-    if (shiftModelNames.includes(shiftModel)) {
-      const isGroupValid =
-        typeof group === "number" &&
-        group <= shiftModelNumberOfGroups[shiftModel] &&
-        group >= 0;
-
-      window.localStorage.setItem(
-        "settings",
-        JSON.stringify({
-          didSelectModel: true,
-          shiftModel,
-          group: isGroupValid ? group : 0,
-        }),
-      );
-    }
-  }, [url, shiftModel, group]);
 }
