bae9cc7211c6d1a5846dea8ca6b4243871ac0af9
Christopher Astfalk <christopher.astfalk@icloud.com>
2024-05-27 17:07:49 +0200

    chore(preact): small fixes and styling changes

--- src/pages/_404.tsx
+++ src/pages/_404.tsx
@@ -1,6 +1,8 @@
+import styles from "./Home/style.module.css";
+
 export function NotFound() {
   return (
-    <section>
+    <section class={styles.not_found}>
       <h1>404: Not Found</h1>
       <p>It's gone :(</p>
     </section>
