d82f46f7e148d499c4ae4b70f9631a8757a84c34
Christopher Astfalk <christopher.astfalk@icloud.com>
2024-09-21 17:54:22 +0200

    feat(calendar): add scroll to element

--- src/pages/Calendar/index.tsx
+++ src/pages/Calendar/index.tsx
@@ -4,6 +4,7 @@ License:
 This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
 the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
+import { useEffect } from "preact/hooks";
 import { Router, Route, useLocation } from "preact-iso";
 
 import CurrentMonth from "./CurrentMonth";
@@ -13,7 +14,9 @@ import { shiftModelNames } from "../../../lib/constants";
 import { ShiftModelKeys } from "../../../lib/shifts";
 
 export default function CalendarRouter({ rest }: { rest: string }) {
-  const { route } = useLocation();
+  const { route, url } = useLocation();
+  useHashScroll(url);
+
   if (!shiftModelNames.includes(rest.split("/")[1] as ShiftModelKeys)) {
     route("/", true);
     return null;
@@ -33,4 +36,20 @@ function Redirector() {
   const { route } = useLocation();
   route("/", true);
   return null;
+}
+
+/**
+ * Replicate the scroll to element with ID in hash behaviour of the browser.
+ * @param pathWithHash  URL (path, query & hash) where to scroll to.
+ */
+function useHashScroll(pathWithHash: string) {
+  useEffect(() => {
+    const url = new URL(pathWithHash, window.location.href);
+    if (url.hash.length <= 1) return;
+
+    const element = document.getElementById(url.hash.replace(/^#/, ""));
+    if (!element) return;
+
+    element.scrollIntoView();
+  }, [pathWithHash]);
 }
